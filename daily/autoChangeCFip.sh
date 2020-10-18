51920b9602526569eb4426d41ea4a4e769a2a545
aknightive <1248597254@qq.com>
2020-10-18 14:52:42 +0800

    升级了下

--- daily/autoChangeCFip.sh
+++ daily/autoChangeCFip.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -e 
 #自用的把选择的cfip倒入qv2ray的配置当中
+#cd /home/aknightive/Downloads/fping-4.2/src 
+#sudo  bash cf.sh
+#cd ~
+#sleep 5
+processid=`ps -ef | grep qv2ray | grep -v grep |awk '{print $2}'|paste -d " " -s`
+for var1 in $processid
+do
+				kill $var1
+done
+
 keyword="heroku kinTaiWan rusiacf ibm kinAmerica"
 cfIP=$(sed 's/\./\\./g' ~/myping.txt)
 for var in $keyword
@@ -9,4 +19,3 @@ for var in $keyword
 							  sed -r -i '11,40 s/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/'$cfIP'/g' ~/.config/qv2ray/connections/$filename.qv2ray.json
 
 				done
-
